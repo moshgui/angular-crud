9525beb891f76f6d66ebcc6371ecadf5922e075f
moshgui <guilherme14_pvh@hotmail.com>
2023-01-15 20:58:42 -0400

    component footer created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,17 +7,27 @@ import { HeaderComponent } from './components/templates/header/header.component'
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/templates/footer/footer.component';
+import { NavComponent } from './components/templates/nav/nav.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
